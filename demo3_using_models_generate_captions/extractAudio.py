4db196816137aa1a2f6327128647ed84e3e5e4a5
汪志鹏 <13476859295@163.com>
2023-04-24 15:10:03 +0800

    add translator

--- demo3_using_models_generate_captions/extractAudio.py
+++
@@ -1,9 +0,0 @@
-import ffmpeg
-
-def extractAudio(input_file, output_file):
-    stream = ffmpeg.input(input_file)
-    audio = stream.audio
-    audio = ffmpeg.output(audio, output_file)
-    ffmpeg.run(audio)
-
-extractAudio("./data/bda-030.mp4","./data/bda-030.mp3")
--- demo3_using_models_generate_captions/main.py
+++
@@ -1,4 +0,0 @@
-from extractAudio import extractAudio
-
-if __name__ == "__main__":
-    extractAudio(r"./data/bda-030.mp4",r"./data/bda-030.mp3")
